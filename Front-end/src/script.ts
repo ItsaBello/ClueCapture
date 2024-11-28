f18fc0defda8a3a2efe2fdb2fbad306ef3546067
Luke-Knaap <Luke.vd.knaap@gmail.com>
2024-11-28 12:22:13 +0100

    Typescript config toegevoegd. ERD diagram toegevoegd.

---
+++ Front-end/src/script.ts
@@ -0,0 +1,13 @@
+document.write("This is a JS code");
+fetch("http://localhost:3000/getData", {
+	method: "GET" // Specify the HTTP method
+});
+
+//1. fetch atfhandelen oftwel de response die gegeven is een functie maken die check of alles klopt in de back-end
+//2. in website routes staat res.send ... (is de response).
+//3. response is String
+//4. haal de response uit de json --> return response.json()
+//5. deel de response in de kleinste delen bv. response.status / !response.ok
+//6. return response.json();
+//7. .then (data =>              oftewel wat ga je doen met de opgevraagde data
+//8. Je wilt de String uit website routes op de website zien.
