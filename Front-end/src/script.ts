52882a4fb605a71375a0203da5756b312d2cf8aa
Roy Bruinsma <Roy.bruinsma@student.hu.nl>
2024-12-03 11:46:00 +0100

    enter hint veld

--- Front-end/src/script.ts
+++ Front-end/src/script.ts
@@ -1,7 +1,14 @@
-// document.write("This is a JS code");
-// fetch("http://localhost:3000/getData", {
-// 	method: "GET" // Specify the HTTP method
-// });
+const inputField = document.getElementById('inputField') as HTMLInputElement;
+
+function handleSubmit(event: any) {
+  event.preventDefault();
+  const inputFieldValue = inputField.value;
+  if (inputFieldValue.trim() === '') {
+    console.log('No hint entered');
+  } else {
+    console.log('Hint entered:', inputFieldValue);
+  }
+}
 
 //1. fetch atfhandelen oftwel de response die gegeven is een functie maken die check of alles klopt in de back-end
 //2. in website routes staat res.send ... (is de response).
