0bff4d60895f0daf600b1e4d40b386c5c78336a4
leraisa <larissabakkerofficial@gmail.com>
2024-12-12 13:18:04 +0100

    Begin scherm toegevoegd en userName

---
+++ Front-end/src/startScreen.ts
@@ -0,0 +1,16 @@
+const form = document.getElementById('username-form') as HTMLFormElement;
+
+
+form.addEventListener('submit', function(event) {
+    console.log ("test")
+    event.preventDefault(); // Prevent that the page loads again
+    const nameInput = document.getElementById('name') as HTMLInputElement;
+    const name = nameInput.value;// Gets the name
+    if (name.trim() !== "") {
+        localStorage.setItem('playerName', name); // Naam opslaan
+        window.location.href = 'boardGame.html';
+    } else {
+        localStorage.setItem('playerName', 'Player one'); // Standaardnaam opslaan
+        window.location.href = 'boardGame.html';
+    }
+});
