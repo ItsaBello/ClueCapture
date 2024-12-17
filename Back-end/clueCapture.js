08552ddb5d8e6d132b3ecf8d45bf7b05c21d06df
Siep v Zutphen <vanzutphensiep@gmail.com>
2024-12-17 12:34:37 +0100

    Database gekoppeld aan de applicatie, uitleggen aan de rest hoe ze de database koppelen.

--- Back-end/clueCapture.js
+++ Back-end/clueCapture.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors'); // Import CORS
 const websiteRoutes = require('./routes/websiteRoutes.js'); // Ensure the path is correct
+const sequelize = require('./database.js'); // Import and use Sequelize (ORM)
 
 const app = express()
 const port = process.env.PORT || 3000; // Server port thats being used
@@ -10,3 +11,12 @@ app.use(express.json({ limit: '10mb' })); // JSON format and size
 app.use(cors());
 // Use website routes
 app.use(websiteRoutes);
+
+//initialize a synchronization with sequelize
+sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+    });
+}).catch(err => {
+    console.error('Unable to sync database:', err);
+});
