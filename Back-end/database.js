fc7fe33d85a9145f035398ed5906802317be11a5
Roy Bruinsma <Roy.bruinsma@student.hu.nl>
2024-12-17 12:51:23 +0100

    server en sql met sequelize opgezset

--- Back-end/database.js
+++ Back-end/database.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
 
+const port = 3306;
+
 // Replace these with your actual database credentials
-const sequelize = new Sequelize('ClueCapture', 'root', 'test123', {
+const sequelize = new Sequelize('ClueCapture', 'root', 'Roy112', {
     host: 'localhost',
     dialect: 'mysql',
-    port: '3306' // This indicates you're using MySQL
+    port: port // This indicates you're using MySQL (default port 3306)
 });
 
 // Test the connection
 sequelize.authenticate()
     .then(() => {
-        console.log('Connection has been established successfully.');
+        console.log('Connection has been established for SQL on the default port:' + port);
     })
     .catch(err => {
         console.error('Unable to connect to the database:', err);
